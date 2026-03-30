359b5672efe94ebebb977666091372bb827c11f3
Luftschloss06 <adamrizkyramadhan33@gmail.com>
2026-03-30 11:13:00 +0700

    Bootstrap Ui

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,14 +6,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        // $name = 'Adam';
-        // $email = 'adamrizkyramadhan33@gmail.com'; 
-        $data = [
-            'title' => 'Prodi SI UNJA',
-            'name' => 'Acikiwir',
-            'email' => 'adamrizkyramadhan33@gmail.com' 
-        ];
-        return view('home', compact('data'));
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home'); 
 Route::get('/well', function(){return view('welcome');});
 Route::get('/posts', [PostController::class, 'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
