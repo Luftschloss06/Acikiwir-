28b81dca0e2a16808593f1c5b034548c8ea69cc8
Luftschloss06 <adamrizkyramadhan33@gmail.com>
2026-04-06 11:15:19 +0700

    autentikasi

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,18 @@
 use App\http\Controllers\HomeController; 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 //Route::get('/', function () {
 //  //return view('welcome');
 //    return "Sistem Informasi - Route";
 //});
+
+
 Route::get('/', [HomeController::class, 'index'])->name('home'); 
 Route::get('/well', function(){return view('welcome');});
 Route::get('/posts', [PostController::class, 'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/proteksi-1', [App\Http\Controllers\HomeController::class, 'proteksi_1'])->name('proteksi.1')->middleware('auth');
